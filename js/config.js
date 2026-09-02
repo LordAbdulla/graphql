7260adcce76be57b875277fa175620ba3b31fc5e
A B D U L L A H . <74745688+LordAbdulla@users.noreply.github.com>
2026-09-02 21:37:38 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,3 +5,4 @@ export const GRAPHQL_URL = `https://${DOMAIN}/api/graphql-engine/v1/graphql`;
 
 export const MODULE_PATH = '/bahrain/bh-module%';
 export const PISCINE_JS_EXCLUDE_PATH = '/bahrain/bh-module/piscine-js/%';
+export const PISCINE_RUST_EXCLUDE_PATH = '/bahrain/bh-module/piscine-rust/%';
